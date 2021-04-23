4c4fa18f4d74b35963a69930cce412dba303c144
josgoo <10632743+josgoo@users.noreply.github.com>
2021-04-23 14:07:58 -0400

    delete stuff

--- server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "cis550-final-project.cmtv07pk5rbt.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "",
-  database: "main"
-};
